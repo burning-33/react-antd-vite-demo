f583fbc59f138b1c615e96f9b1baebb77e5801d6
burning-33 <977690289@qq.com>
2023-11-07 13:30:59 +0800

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2023: true },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'eslint-config-mature/prettier',
+    'eslint-config-mature',
+    'eslint-config-mature/ts',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    esmaFeatures: {
+      "jsx": true
+    },
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['react', 'react-refresh'],
+  rules: {
+    "semi": 0,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  }
+}
